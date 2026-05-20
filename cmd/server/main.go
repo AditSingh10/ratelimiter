aa6c287144c6a5253c8663c889ffbbb8e1c215c5
AditSingh10 <singh.adit6@gmail.com>
2026-05-20 16:53:20 -0700

    feat: add token_bucket impl to main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,19 +5,28 @@ import (
 	"log"
 	"net"
 
+	"github.com/AditSingh10/ratelimiter/internal/limiter"
 	pb "github.com/AditSingh10/ratelimiter/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
 	pb.UnimplementedRateLimiterServer
+	l limiter.Limiter
 }
 
-func (c *server) Allow(context.Context, *pb.AllowRequest) (*pb.AllowResponse, error) {
-	out := new(pb.AllowResponse)
-	out.Allowed = true
-	out.Remaining = 99
-	return out, nil
+func (c *server) Allow(ctx context.Context, req *pb.AllowRequest) (*pb.AllowResponse, error) {
+	allowed, remaining, resetAt, err := c.l.Allow(ctx, req.ClientId)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.AllowResponse{
+		Allowed:       allowed,
+		Remaining:     remaining,
+		ResetAtUnix:   resetAt.Unix(),
+		AlgorithmUsed: "token_bucket",
+	}, nil
 }
 
 func main() {
@@ -27,7 +36,10 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-	pb.RegisterRateLimiterServer(grpcServer, &server{})
+	// add reflection grpcurl works
+	reflection.Register(grpcServer)
+
+	pb.RegisterRateLimiterServer(grpcServer, &server{l: limiter.NewTokenBucket(100, 10)})
 
 	log.Println("server listening on :50051")
 	if err := grpcServer.Serve(lis); err != nil {

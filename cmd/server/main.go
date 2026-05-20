7392693ab93d5b0902019bc15652e8662277455f
AditSingh10 <singh.adit6@gmail.com>
2026-05-20 14:28:10 -0700

    feat: rateLimiterServer TCP connection, methods unimplemented

---
+++ cmd/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/AditSingh10/ratelimiter/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedRateLimiterServer
+}
+
+func (c *server) Allow(context.Context, *pb.AllowRequest) (*pb.AllowResponse, error) {
+	out := new(pb.AllowResponse)
+	out.Allowed = true
+	out.Remaining = 99
+	return out, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051") // use net to open the server to listen via TCP on port 50051
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err) // what is Fatalf and %v
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterRateLimiterServer(grpcServer, &server{}) // dont understand how server and grpcserver fit into this
+
+	log.Println("server listening on :50051")
+	if err := grpcServer.Serve(lis); err != nil { // what is Serve
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}

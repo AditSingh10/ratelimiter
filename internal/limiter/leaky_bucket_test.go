14fddd71eb9f1e57bbca2d3a4f9a39df51994c52
AditSingh10 <singh.adit6@gmail.com>
2026-05-27 16:30:48 -0700

    test: leaky_bucket

---
+++ internal/limiter/leaky_bucket_test.go
@@ -0,0 +1,74 @@
+package limiter
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestLBAllow_BasicAllow(t *testing.T) {
+	lb := NewLeakyBucket(1, time.Second)
+
+	// Background is a no-op context for tests
+	allowed, _, _, err := lb.Allow(context.Background(), "client1")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if !allowed {
+		t.Errorf("expected allowed=true, got false")
+	}
+
+}
+
+func LBTestAllow_AllowExhausted(t *testing.T) {
+	// create a bucket with capacity 1, rate 1
+	lb := NewLeakyBucket(1, 1)
+
+	// Background is a no-op context for tests
+	allowed1, _, _, err1 := lb.Allow(context.Background(), "client1")
+	allowed2, _, _, err2 := lb.Allow(context.Background(), "client1")
+
+	if err1 != nil {
+		t.Fatalf("unexpected error1: %v", err1)
+	}
+	if !allowed1 {
+		t.Errorf("expected allowed1=true, got false")
+	}
+
+	if err2 != nil {
+		t.Fatalf("unexpected error2 error?: %v", err2)
+	}
+	if allowed2 {
+		t.Errorf("expected allowed2=false, got true")
+	}
+}
+
+// concurrency testing
+func LBTestAllow_Concurrent(t *testing.T) {
+	lb := NewLeakyBucket(10, 100)
+	var wg sync.WaitGroup
+	var allowedCount atomic.Int64
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			allowed, _, _, err := lb.Allow(context.Background(), "client1")
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+			if allowed {
+				allowedCount.Add(1)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	if allowedCount.Load() > 10 {
+		t.Errorf("expected at most 10 allowed, got %d", allowedCount.Load())
+	}
+}
